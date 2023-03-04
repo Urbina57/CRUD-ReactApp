31be65ad65fa76af59acce263483f672769bf6ca
Poncho <urbina_57@outlook.com>
2023-03-04 11:20:33 -0600

    header added

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import Headroom from 'react-headroom'
+import '../App.css';
+
+export function Header () {
+
+    return(
+    <Headroom className={'header'} >
+        <h3>React-Crud</h3>
+    </Headroom>
+    )
+}
